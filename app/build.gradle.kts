c52490b022b8b10ebfa38e080a91d81eedfbf175
Vadim Uzbekov <eldarovich99@mail.ru>
2023-06-11 23:05:51 +0200

    Build fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.0"
+        kotlinCompilerExtensionVersion = "1.4.3"
     }
     /*packagingOptions {
         resources {
@@ -62,13 +62,13 @@ android {
 }
 
 dependencies {
-    val composeUiVersion = "1.4.0-rc01"
-    implementation ("androidx.core:core-ktx:1.9.0")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0")
-    implementation ("androidx.activity:activity-compose:1.6.1")
+    val composeUiVersion = "1.4.3"
+    implementation ("androidx.core:core-ktx:1.10.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation ("androidx.activity:activity-compose:1.7.2")
     implementation ("androidx.compose.ui:ui:$composeUiVersion")
     implementation ("androidx.compose.ui:ui-tooling-preview:$composeUiVersion")
-    implementation ("androidx.compose.material:material:1.3.1")
+    implementation ("androidx.compose.material:material:1.4.3")
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
@@ -79,7 +79,7 @@ dependencies {
     detektPlugins("com.twitter.compose.rules:detekt:0.0.26")
 
     // Coroutines
-    val coroutinesVersion = "1.7.0-Beta"
+    val coroutinesVersion = "1.6.1"
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
 
